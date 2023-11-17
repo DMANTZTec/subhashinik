4c639a61cd34f04b5f3edbcb648ea24c70ec557e
Subhashini-kolluri12 <115128474+Subhashini-kolluri12@users.noreply.github.com>
2023-11-17 16:34:41 +0530

    Delete args and kwargs program.py

--- args and kwargs program.py
+++
@@ -1,8 +0,0 @@
-def pizza(size,*toppings,**details):
-    print("pizzaa toppings")
-    for topping in toppings:
-            print(topping)
-    print("orderdetails")
-    for key,value in details.items():
-          print(f'{key}:{value}')
-pizza("large","pepperion","extra chesse","onions","mushrooms",gachibowli='pizzavibes',tip=20)
