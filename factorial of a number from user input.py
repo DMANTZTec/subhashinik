459e778173a0278f3d62158d1363ab8d855111b0
Subhashini-kolluri12 <115128474+Subhashini-kolluri12@users.noreply.github.com>
2023-11-17 16:47:06 +0530

    Delete factorial of a number from user input.py

--- factorial of a number from user input.py
+++
@@ -1,8 +0,0 @@
-#Write a program to take number as input from user and print it's factorial
-def fac(n):
-    if n==1:
-        return 1
-    else:
-        return n* fac(n-1)
-n=int(input("enter number : "))
-print(fac(n))
