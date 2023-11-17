8ede1c14fe2f8734add4dd3254c08c0a3ba0a5d0
Subhashini-kolluri12 <115128474+Subhashini-kolluri12@users.noreply.github.com>
2023-11-17 16:54:28 +0530

    Delete sets intersection.py

--- sets intersection.py
+++
@@ -1,9 +0,0 @@
-#Write a program to take values for two sets and get intersection of that sets.
-set1 = int(input("enter number : "))
-set2 = int(input("enter number : "))
-user_input1=set1.split()
-user_input2=set2.split()
-user_set1=set(user_input1)
-user_set2=set(user_input2)
-intersection=user_set1|user_set2
-print(intersection)
